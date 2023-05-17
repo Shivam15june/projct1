5f12c5a983f04ec75a79a9e0e316bcdf12d46f2f
shivam <shivamsri15june@gmail.com>
2023-05-17 11:45:44 +0530

    feat changes

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cipl
+ *
+ */
+module practise1 {
+}
---
+++ src/practise1/Day1.java
@@ -0,0 +1,12 @@
+package practise1;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hey this is my first class!!!!!!!!!!!!");
+
+	}
+
+}
