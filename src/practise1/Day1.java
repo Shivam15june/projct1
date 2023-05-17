3651e1048bb3ebfd1e184e7a7a6fac9a4fd3e029
shivam <shivamsri15june@gmail.com>
2023-05-17 11:47:50 +0530

    changes

--- src/practise1/Day1.java
+++ src/practise1/Day1.java
@@ -3,10 +3,10 @@ package practise1;
 public class Day1 {
 
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("Hey this is my first class!!!!!!!!!!!!");
 
+		System.out.println("Hey");
 	}
 
 }
